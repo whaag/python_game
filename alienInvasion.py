33b0f53332f27e1c9d3e0f781b3a29ca8aa09521
Eduardo Athayde <eduardo.a.athayde@gmail.com>
2017-09-30 23:35:09 +0100

    fix bullets issue

--- alienInvasion.py
+++ alienInvasion.py
@@ -3,6 +3,8 @@ import game_functions as gf
 
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
+
 
 def run_game():
     pygame.init()
@@ -10,13 +12,15 @@ def run_game():
     screen = pygame.display.set_mode((game_settings.screen_width, game_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #make a ship
+    # make a ship
     ship = Ship(screen, game_settings)
+    bullets = Group()
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ship, game_settings, screen, bullets)
         ship.update()
-        gf.update_screen(game_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(game_settings, screen, ship, bullets)
 
 
 run_game()
--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-class Settings():
+class Settings:
     """"A class to store all settings for the game."""
 
     def __init__(self):
@@ -15,4 +15,5 @@ class Settings():
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
-        self.bullet_speed_factor = 3
+        self.bullet_speed_factor = 1
+        self.max_bullets_allowed = 3
